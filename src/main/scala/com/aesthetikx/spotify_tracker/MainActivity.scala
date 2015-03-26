754be69198311b6e4b9c72ee106fab49eccc0bc7
John DeSilva <desilvjo@umich.edu>
2015-03-26 14:55:05 -0400

    Initial commit

---
+++ src/main/scala/com/aesthetikx/spotify_tracker/MainActivity.scala
@@ -0,0 +1,12 @@
+package com.aesthetikx.spotify_tracker
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity extends Activity {
+
+  override def onCreate(savedInstanceState: Bundle) {
+
+  }
+
+}
